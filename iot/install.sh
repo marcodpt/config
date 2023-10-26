6e3c2d8421ca97c8c2cb919c9b99621dd69db73f
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2023-10-26 14:54:22 -0300

    updated dependencies

--- iot/install.sh
+++ iot/install.sh
@@ -11,11 +11,11 @@ apt autoremove -y
 # apt install pkg-config -y
 # apt install libudev-dev -y
 
-wget https://github.com/marcodpt/toledo/releases/download/0.1.0/default.toledo-x86_64-linux
-mv default.toledo-x86_64-linux /usr/bin/toledo
+wget https://github.com/marcodpt/toledo/releases/download/0.1.0/toledo-x86_64-unknown-linux-musl
+mv toledo-x86_64-unknown-linux-musl /usr/bin/toledo
 chmod ugo+x /usr/bin/toledo
 
-wget https://github.com/marcodpt/rawprinter/releases/download/0.1.1/rawprinter-x86_64-linux
+wget https://github.com/marcodpt/rawprinter/releases/download/0.1.2/rawprinter-x86_64-linux
 mv rawprinter-x86_64-linux /usr/bin/rawprinter
 chmod ugo+x /usr/bin/rawprinter
 
@@ -23,20 +23,20 @@ wget https://github.com/marcodpt/minirps/releases/download/0.1.1/minirps-x86_64-
 mv minirps-x86_64-unknown-linux-musl /usr/bin/minirps
 chmod ugo+x /usr/bin/minirps
 
-mkdir -p /opt/hardware
+mkdir -p /opt/iot
 
 wget https://raw.githubusercontent.com/marcodpt/config/main/iot/config.toml
-mv config.toml /opt/hardware/
+mv config.toml /opt/iot/
 
 wget --no-check-certificate https://192.168.0.5/index.php/download/iot_cert
-mv iot_cert /opt/hardware/ssl.crt
+mv iot_cert /opt/iot/ssl.crt
 
 wget --no-check-certificate https://192.168.0.5/index.php/download/iot_key
-mv iot_key /opt/hardware/ssl.key
+mv iot_key /opt/iot/ssl.key
 
 wget https://raw.githubusercontent.com/marcodpt/config/main/iot/rc.local
 mv "rc.local" "/etc/rc.local"
 
 rawprinter --vendor-id 0x0a5f --device-id 0x000a &
 toledo /dev/ttyUSB0 --lang pt --unit Kg --min-weight 0.01 &
-minirps -f /opt/hardware/config.toml &
+minirps -f /opt/iot/config.toml &
