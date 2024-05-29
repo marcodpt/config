adf19ba3de26bdb34e5b183b3ca413b8c055847e
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-05-29 15:16:27 -0300

    Fix scripts

--- aws/daily.sh
+++ aws/daily.sh
@@ -4,8 +4,8 @@ echo "***** DAILY *****" > /home/ubuntu/daily.log
 
 git --work-tree=/home/ubuntu/config/ --git-dir=/home/ubuntu/config/.git/ pull >> /home/ubuntu/daily.log
 
-/home/ubuntu/config/scripts/backup.sh -c -d /home/ubuntu/backup db01 /home/ubuntu/backup/.mysql >> /home/ubuntu/daily.log
-/home/ubuntu/config/scripts/clean.sh -m 35 -d /home/ubuntu/backup db01 >> /home/ubuntu/daily.log
-
-/home/ubuntu/config/scripts/backup.sh -c -d /home/ubuntu/backup db02 /home/ubuntu/backup/.mysql >> /home/ubuntu/daily.log
-/home/ubuntu/config/scripts/clean.sh -m 35 -d /home/ubuntu/backup db02 >> /home/ubuntu/daily.log
+for DB in "$@"
+do
+  /home/ubuntu/config/scripts/backup.sh -c -d /home/ubuntu/backup $DB /home/ubuntu/backup/.mysql >> /home/ubuntu/daily.log
+  /home/ubuntu/config/scripts/clean.sh -m 35 -d /home/ubuntu/backup $DB >> /home/ubuntu/daily.log
+done
--- aws/update.sh
+++ aws/update.sh
@@ -2,8 +2,11 @@
 
 echo "***** UPDATE *****" > /home/ubuntu/update.log
 
-/home/ubuntu/config/scripts/backup.sh -c -d /home/ubuntu/backup db03 /home/ubuntu/backup/.mysql >> /home/ubuntu/update.log
-/home/ubuntu/config/scripts/clean.sh -m 5 -d /home/ubuntu/backup db03 >> /home/ubuntu/update.log
+for DB in "$@"
+do
+  /home/ubuntu/config/scripts/backup.sh -d /home/ubuntu/backup $DB /home/ubuntu/backup/.mysql >> /home/ubuntu/daily.log
+  /home/ubuntu/config/scripts/clean.sh -m 5 -d /home/ubuntu/backup $DB >> /home/ubuntu/daily.log
+done
 
 apt update >> /home/ubuntu/update.log
 apt upgrade -y >> /home/ubuntu/update.log
