27953a2355ea1f9ebf8fd7be37d024a0269fc4b1
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-06-03 15:04:26 -0300

    kill process before restart

--- iot/startup.sh
+++ iot/startup.sh
@@ -2,6 +2,10 @@
 
 echo "***** STARTUP *****"
 
+kill $(ps -ef | grep rawprinter | head -1 | awk '{print $2}')
+kill $(ps -ef | grep serialscale | head -1 | awk '{print $2}')
+kill $(ps -ef | grep minirps | head -1 | awk '{print $2}')
+
 rawprinter --vendor-id 0x0a5f --device-id 0x000a &
 serialscale /dev/ttyUSB0 --lang pt --unit Kg --min-weight 0.01 &
 minirps -f /etc/iot/config.toml &
