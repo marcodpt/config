a2fc709bcad4ec2d4b5a4e5fc88da622ec9c844e
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-06-03 15:34:44 -0300

    Added sleep for bg process

--- iot/startup.sh
+++ iot/startup.sh
@@ -9,3 +9,5 @@ kill $(ps -ef | grep minirps | head -1 | awk '{print $2}')
 rawprinter --vendor-id 0x0a5f --device-id 0x000a &
 serialscale /dev/ttyUSB0 --lang pt --unit Kg --min-weight 0.01 &
 minirps -f /etc/iot/config.toml &
+
+sleep 0.1
