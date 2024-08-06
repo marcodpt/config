758a123bfe16dbad3f5830eb01415ee20239026d
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-08-06 11:08:21 -0300

    support argox

--- iot/startup.sh
+++ iot/startup.sh
@@ -14,7 +14,7 @@ if [[ $(ps -ef | grep minirps | wc -l) -gt 1 ]]; then
   kill $(ps -ef | grep minirps | head -1 | awk '{print $2}')
 fi
 
-ID=$(lsusb | grep -w Zebra | awk '{print $6}' | awk -F ':' '{print$2}')
+ID=$(lsusb | grep -e Zebra -e ARGOX | awk '{print $6}' | awk -F ':' '{print$2}')
 
 rawprinter --vendor-id 0x0a5f --device-id 0x$ID &
 serialscale /dev/ttyUSB0 --lang pt --unit Kg --min-weight 0.01 &
