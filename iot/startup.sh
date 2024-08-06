6535c2799b0620e835c97a96da0eecc1ad11be9d
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-08-06 11:24:56 -0300

    Fix

--- iot/startup.sh
+++ iot/startup.sh
@@ -14,9 +14,11 @@ if [[ $(ps -ef | grep minirps | wc -l) -gt 1 ]]; then
   kill $(ps -ef | grep minirps | head -1 | awk '{print $2}')
 fi
 
-ID=$(lsusb | grep -e Zebra -e ARGOX | awk '{print $6}' | awk -F ':' '{print$2}')
+ID=$(lsusb | grep -e Zebra -e ARGOX | awk '{print $6}')
+VENDOR=$(echo $ID | awk -F ':' '{print$1}')
+DEVICE=$(echo $ID | awk -F ':' '{print$2}')
 
-rawprinter --vendor-id 0x0a5f --device-id 0x$ID &
+rawprinter --vendor-id 0x$VENDOR --device-id 0x$DEVICE &
 serialscale /dev/ttyUSB0 --lang pt --unit Kg --min-weight 0.01 &
 minirps -f /etc/iot/config.toml &
 
