3c70a7e0d785dea8530d963ff10ff5260228a91b
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-08-20 16:23:42 -0300

    try fix vim

--- marco/config.sh
+++ marco/config.sh
@@ -47,4 +47,4 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-vim +'PlugInstall --sync' +'PlugUpdate --sync' +'PlugClean! --sync' +qall
+vim -e +PlugInstall +PlugUpdate +PlugClean! +qall
