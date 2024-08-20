2725576d108c740eac1d5e62ed0b1a44943e361f
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-08-20 19:09:14 -0300

    auto update

--- marco/install.sh
+++ marco/install.sh
@@ -12,3 +12,10 @@ apt install pass -y
 apt install sqlite3 -y
 apt install claws-mail -y
 apt install ripgrep -y
+
+RC="/etc/rc.local"
+echo "#!/bin/bash" > $RC
+echo "apt update" >> $RC
+echo "apt upgrade -y" >> $RC
+echo "apt autoremove -y" >> $RC
+echo "exit 0" >> $RC
