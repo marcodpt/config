217fba7f272744bb0d2fba858352b74a2a3933ea
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-08-20 18:16:04 -0300

    better sync

--- marco/config.sh
+++ marco/config.sh
@@ -18,11 +18,37 @@ git config --global user.email "marcodpt@protonmail.com"
 git config --global user.name "Marco Di Pillo Tomic"
 git config --global push.default simple
 
-#PASS
-STORE="$HOME/.password-store"
-if [ ! -d $STORE ]; then
-  git clone git@gitlab.com:marcodpt/pass.git $STORE
-fi
+#PROJECTS
+sync () {
+  if [ $2 == "" ]; then
+    2="github"
+  fi
+  if [ $3 == "" ]; then
+    3="Desktop/$1" 
+  fi
+
+  echo "******* $2@$1 $3 *********"
+
+  if [ ! -d $FOLDER ]; then
+    git clone git@$2.com:marcodpt/$1.git $HOME/$3
+  else
+    cd $FOLDER
+    git pull
+  fi
+}
+
+sync config
+sync minirps
+sync rawprinter
+sync serialscale
+sync tint
+sync paw
+sync hippo
+sync respect
+sync apitest
+sync rest gitlab
+sync auto gitlab
+sync pass gitlab .password-store
 
 #RUST
 if [ ! -d ~/.rustup ]; then
@@ -47,4 +73,6 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-vim -T dumb -n -i NONE -es -S <(echo 'PluginInstall PlugUpdate PlugClean! qall')
+if [ $0 != "bash" ]; then
+  vim +PlugInstall +PlugUpdate +PlugClean! +qall
+fi
